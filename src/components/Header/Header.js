5a511e97847710a4aa35dfc9a0947f7abf82c054
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-09-08 20:59:15 +0530

    Add design changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,7 @@ const Header = () => {
     return (
         <div className='header-wrapper'>
             <p className='header-text'>Palindrome Birthday</p>
+            <p className='header-sub-text'>Check if your birthday is a palindrome 🥳</p>
         </div>
     )
 }
