c52c117955548bcd5faed4348a9a9db63ff32e0e
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-09-08 20:45:33 +0530

    Add CheckButton component

---
+++ src/components/CheckButton/CheckButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CheckButton.css';
+
+const CheckButton = () => {
+    return (
+        <div className='btn-wrapper'>
+            <button>Check Birthday</button>
+        </div>
+    )
+}
+
+export default CheckButton;
