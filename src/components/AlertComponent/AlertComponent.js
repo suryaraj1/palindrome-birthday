b99304337c36106e7b5381bc3b7654a73fbe3bbd
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-09-08 23:04:02 +0530

    Format Date on alert

--- src/components/AlertComponent/AlertComponent.js
+++ src/components/AlertComponent/AlertComponent.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './AlertComponent.css';
 
+const formatDate = dateObj => {
+    const { day, month, year } = dateObj;
+    return `${day}-${month}-${year}`;
+}
+
 const AlertComponent = ({ isDatePalindrome, dayCount, nextDate }) => {
+    formatDate(nextDate);
     return (
         <div className={`alert-wrapper ${isDatePalindrome && 'success'}`}>
             {isDatePalindrome ? <p>Hurray!! Your birthday is a Palindrome 🎉</p> : 
-            <p>{`The nearest palindrome date is ${nextDate}, you missed by ${dayCount} days`}</p>}
+            <p>{`The nearest palindrome date is ${formatDate(nextDate)}, you missed by ${dayCount} days 😢`}</p>}
         </div>
     )
 }
