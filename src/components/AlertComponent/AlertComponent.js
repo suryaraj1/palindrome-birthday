842ae9cb516e7ffa6ecdcd278c3d495d36c55dcf
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-09-09 09:39:24 +0530

    Add react lottie npm package, lottie animation

--- src/components/AlertComponent/AlertComponent.js
+++ src/components/AlertComponent/AlertComponent.js
@@ -1,19 +1,52 @@
-import React from 'react';
-import './AlertComponent.css';
+import React from "react";
+import Lottie from "react-lottie";
+import animationData from "../../lotties/confetti.json";
+import "./AlertComponent.css";
 
-const formatDate = dateObj => {
+const defaultOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice",
+    },
+};
+
+const formatDate = (dateObj) => {
     const { day, month, year } = dateObj;
     return `${day}-${month}-${year}`;
-}
+};
 
 const AlertComponent = ({ isDatePalindrome, dayCount, nextDate }) => {
     formatDate(nextDate);
     return (
-        <div className={`alert-wrapper ${isDatePalindrome && 'success'}`}>
-            {isDatePalindrome ? <p>Hurray!! Your birthday is a Palindrome 🎉</p> : 
-            <p>{`The nearest palindrome date is ${formatDate(nextDate)}, you missed by ${dayCount} days 😢`}</p>}
+        <div className="alert-container">
+            {isDatePalindrome ? (
+                <>
+                    <div className="confetti-wrapper">
+                        <Lottie
+                            options={defaultOptions}
+                            height={200}
+                            width={200}
+                        />
+                    </div>
+                    <div
+                        className={`alert-wrapper ${
+                            isDatePalindrome && "success"
+                        }`}
+                    >
+                        <p>Hurray!! Your birthday is a Palindrome 🎉</p>
+                    </div>
+                </>
+            ) : (
+                <div className="alert-wrapper">
+                    <p>{`The nearest palindrome date is ${formatDate(
+                        nextDate
+                    )}, you missed by ${dayCount} days 😢`}</p>
+                </div>
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default AlertComponent;
