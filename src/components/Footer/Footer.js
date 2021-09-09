7473294261c0b679594b7bf762c77c6617bdf5d4
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-09-09 21:06:50 +0530

    Add footer, change site title

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,18 +6,15 @@ const Footer = () => {
         <div className='footer-wrapper'>
             <p className='footer-text'>© | 2021 | Suryaraj</p>
             <div className='footer-social-wrapper'>
-                <div className='footer-social'>
+                <a className='footer-social' href="https://twitter.com/BhaduriSuryaraj" target="_blank" rel="noreferrer">
                     <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/twitter/twitter-original.svg" alt="Twitter"/>
-                </div>
-                <div className='footer-social'>
+                </a>
+                <a className='footer-social' href="https://github.com/suryaraj1" target="_blank" rel="noreferrer">
                     <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="Github"/>
-                </div>
-                <div className='footer-social'>
+                </a>
+                <a className='footer-social' href="https://www.linkedin.com/in/suryaraj-bhaduri-a6706b162/" target="_blank" rel="noreferrer">
                     <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" alt="Linkedin"/>
-                </div>
-            </div>
-            <div className='privacy-notice-wrapper'>
-                <p>Privacy Notice - No data is being stored</p>
+                </a>
             </div>
         </div>
     )
